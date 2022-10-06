d08362ae52fe7f499fdce7c45d5cb8a4f2c437b4
Jakub Prach <jakub.prach76@gmail.com>
2022-10-06 22:24:44 +0200

    Added connection to mongoDB and CRUD operations

---
+++ TableBookingAPI/TableBooking/Models/Reservation.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TableBooking.Models;
+
+public class Reservation
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+}
