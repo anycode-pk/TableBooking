62b3adff79ca6cfb31832ff80b61f87945fecda2
Paweł Frankowski <pawelfrankowski42@gmail.com>
2023-04-18 16:30:16 +0200

    DTO

--- TableBookingAPI/TableBooking/Controllers/RestaurantController.cs
+++ TableBookingAPI/TableBooking/Controllers/RestaurantController.cs
@@ -15,37 +15,39 @@ namespace TableBooking.Controllers
         }
         
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAllRestaurants()
         {
-            return Ok(_context.Restaurants.ToList());
+            var restaurants = _context.Restaurants.ToList();
+            return Ok(restaurants);
         }
 
         [HttpGet("/{id}")]
         public IActionResult GetById(int id)
         {
-            var restaurant = _context.Restaurants.Find(id); // szukanie po primary key
+            var restaurant = _context.Restaurants.Find(id);
             return Ok(restaurant);
         }
 
         [HttpPost]
-        public IActionResult Add(string name)
+        public async Task<IActionResult> Add(string name)
         {
             var restaurant = new Restaurant { Name = name };
             _context.Restaurants.Add(restaurant);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(restaurant);
         }
 
-        [HttpDelete("{id:int}")] // kod nie powinien zwracac bledu
+        [HttpDelete("{id:int}")] 
         public async Task<IActionResult> Delete(int id)
         {
             var restaurantToDelete = await _context.Restaurants.FindAsync(id);
             if (restaurantToDelete == null)
-                return NotFound($"Restaurant with Id = {id} not found"); // zwraca 404
-            _context.Restaurants.Remove(restaurantToDelete); // przenies do repozytorium
-            await _context.SaveChangesAsync(); // metoda z EF
-            return Ok(); // zwraca 200
+                return NotFound($"Restaurant with Id = {id} not found");
+            _context.Restaurants.Remove(restaurantToDelete);
+            await _context.SaveChangesAsync();
+            return Ok(restaurantToDelete);
         }
+        
         //[HttpGet("{id}")]
         //public async Task<ActionResult<Restaurant>> Get(string id)
         //{
---
+++ TableBookingAPI/TableBooking/DTOs/RestaurantShortInfoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TableBooking.DTOs;
+
+public class RestaurantShortInfoDTO
+{
+    public string Name { get; set; }
+    public string? Type { get; set; }
+    public string? Description { get; set; }
+    public string? Location { get; set; }
+    public DateTime OpenTime { get; set; }
+    public DateTime CloseTime { get; set; }
+}
