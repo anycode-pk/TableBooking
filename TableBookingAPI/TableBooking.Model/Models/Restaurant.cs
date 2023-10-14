901d4d99effbcf0f3d9f2630e70683b5fe5405ff
Paul Frank <pawelspam42@gmail.com>
2023-10-14 22:31:07 +0200

    Guid as PK

--- TableBookingAPI/TableBooking.Model/Models/Restaurant.cs
+++ TableBookingAPI/TableBooking.Model/Models/Restaurant.cs
@@ -12,7 +12,9 @@ public class Restaurant : Entity
     public string Type { get; set; }
     public string? Description { get; set; }
     public string Location { get; set; }
-    public string? ImageURL { get; set; }
+    public string Phone { get; set; }
+    public string? PrimaryImageURL { get; set; }
+    public string? SecondaryImageURL { get; set; }
     public int Rating { get; set; }
     public Price Price { get; set; }
     public TimeSpan OpenTime { get; set; }
