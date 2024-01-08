c97946c07536e0244e45bbc7ddc41dd94abc7dd9
Kacper Ochnik <kacperochnik01@gmail.com>
2024-01-08 14:22:36 +0100

    dependency fix

--- TableBookingClient/tests/unit/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import HomePage from '@/views/HomeView/HomeView.vue'
-
-describe('HomePage.vue', () => {
-  it('renders home vue', () => {
-    const wrapper = mount(HomePage)
-    expect(wrapper.text()).toMatch('Ready to create an app?')
-  })
-})
